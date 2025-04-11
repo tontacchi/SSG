9ea5191d4d773c23e9dcffb9acbe3a21f8cd827c
tontacchi <andy.tonthat14@gmail.com>
2025-04-11 12:13:08 -0700

    finished site generation functionality 1. block -> HTMLNode conversion functions 2. fixed splitting text bug. more than 1 of same delimiter bug fixed 3. automatic directory generation. automatic public/ removal. recursive file deleting & copying 4. main.sh automatically launches http server

--- src/main.py
+++ src/main.py
@@ -1,18 +1,119 @@
 #---[ Global Imports ]----------------------------------------------------------
-from textnode import TextNode, TextType
+import os
+from   pathlib import Path
+import shutil
+
+from   utils   import extract_title, markdown_to_html_node
 
 #---[ Global Imports ]----------------------------------------------------------
 
 
 #---[ Main Function ]-----------------------------------------------------------
 def main():
-    test = TextNode("hi **bold****two** words")
+    remove_public_dir_files()
 
-    res = split_nodes_delimiter([ test ], "**", TextType.BOLD)
+    static_dir = get_project_dir("static")
+    public_dir = get_project_dir("public")
+    copy_files(static_dir, public_dir)
+
+    content_dir  = get_project_dir("content")
+    template_dir = get_project_dir(".") / "template.html"
+
+    print("")
+    generate_pages_recursively(content_dir, template_dir, public_dir)
 
     return
 
 #---[ Main Function ]-----------------------------------------------------------
+
+def get_project_dir(subdir: str) -> Path:
+    script_dir = Path(__file__).parent.resolve()
+    target_dir = script_dir.parent.resolve() / subdir 
+    if not target_dir.exists():
+        target_dir.mkdir()
+
+    return target_dir
+
+def remove_files(directory: Path) -> None:
+    for file in directory.iterdir():
+        if file.is_dir():
+            remove_files(file)
+            os.rmdir(file)
+        else:
+            os.remove(file)
+
+    return
+
+def copy_files(source_dir: Path, target_dir: Path) -> None:
+    print("copying files:")
+    print(f"source: {source_dir.parent.name}/{source_dir.name}")
+    print(f"dest: {target_dir.parent.name}/{target_dir.name}")
+    for file in source_dir.iterdir():
+        if file.is_dir():
+            new_directory = target_dir / f"{file.name}"
+            print(f"creating directory {new_directory.parent.name}/{new_directory.name}")
+            os.mkdir(new_directory)
+            copy_files(file, new_directory)
+        else:
+            print(f"copying {file.name}")
+            shutil.copy(file, target_dir / f"{file.name}")
+
+    return
+
+def remove_public_dir_files() -> None:
+    public_dir = get_project_dir("public")
+    remove_files(public_dir)
+
+    return
+
+def generate_page(src_path: Path, template_path: Path, dest_path: Path) -> bool:
+    short_src_path      = f"{src_path.parent.parent.name}/{src_path.parent.name}/{src_path.name}"
+    short_template_path = f"{template_path.parent.parent.name}/{template_path.parent.name}/{template_path.name}"
+    short_dest_path     = f"{dest_path.parent.parent.name}/{dest_path.parent.name}/{dest_path.name}"
+    message = f"Generating page from {short_src_path} to {short_dest_path} using {short_template_path}"
+
+    print(message)
+
+    markdown = ""
+    with src_path.open("r") as inFile:
+        markdown = inFile.read()
+
+        if markdown == "":
+            return False
+
+    template = ""
+    with template_path.open('r') as inFile:
+        template = inFile.read()
+
+        if template == "":
+            return False
+
+    root_node = markdown_to_html_node(markdown)
+
+    content = root_node.to_html()
+    title = extract_title(markdown)
+
+    html_str = template.replace("{{ Title }}", title)
+    html_str = html_str.replace("{{ Content }}", content)
+
+    with dest_path.open('w') as outFile:
+        outFile.write(html_str)
+
+    return True
+
+def generate_pages_recursively(content_path: Path, template_path: Path, dest_path: Path) -> None:
+    for path in content_path.iterdir():
+        if path.is_dir():
+            new_dest = dest_path / path.name
+            if not new_dest.exists():
+                new_dest.mkdir()
+
+            generate_pages_recursively(path, template_path, new_dest)
+        elif path.name.endswith(".md"):
+            file_dest_path = dest_path / str(path.name).replace(".md", ".html")
+            generate_page(path, template_path, file_dest_path)
+
+    return
 
 
 #---[ Entry ]-------------------------------------------------------------------
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,78 @@
 #---[ Global Imports ]----------------------------------------------------------
-from   textnode import TextNode, TextType
+import re
+
+# nodes
+from   textnode import TextNode
 from   htmlnode import HTMLNode
 from   leafnode import LeafNode
+from   parentnode import ParentNode
 
-import re
+# node types
+from   textnode import TextType
+from   block    import BlockType
 
 #---[ Global Imports ]----------------------------------------------------------
 
-def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
+def extract_title(markdown: str) -> str:
+    if not markdown: raise ValueError("empty markdown")
+
+    title = ""
+
+    first_line = markdown.split('\n')[0]
+    if first_line[0] != "#":
+        raise ValueError("markdown does not begin with a heading #")
+    
+    index = 0
+    while first_line[index] == "#":
+        index += 1
+
+    if first_line[index + 1:] == title:
+        raise ValueError("empty heading")
+
+    return first_line[index + 1:]
+
+#---[ str, TextNode, HTMLNode Conversion ]--------------------------------------
+def text_to_text_nodes(text: str) -> list[TextNode]:
+    '''
+    Splits inline markdown into text, bold, italic, and code TextNodes
+    '''
+    type_delimiter_map = {
+        TextType.BOLD: "**",
+        TextType.ITALIC: "_",
+        TextType.CODE: "`"
+    }
+
+    node = TextNode(text)
+    node_list = [node]
+
+    prev_list = []
+
+    while prev_list != node_list:
+        if prev_list == node_list: break
+        prev_list = node_list.copy()
+
+        for text_type, delimiter in type_delimiter_map.items():
+            node_list = split_nodes_delimiter(node_list, delimiter, text_type)
+
+        node_list = split_nodes_image(node_list)
+        node_list = split_nodes_link(node_list)
+
+    return node_list
+
+def text_to_children(text: str) -> list[LeafNode]:
+    textnode_list = text_to_text_nodes(text)
+    child_node_list = []
+
+    for text_node in textnode_list:
+        temp = text_node_to_html_node(text_node)
+        child_node_list.append(temp)
+
+    return child_node_list
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    '''
+    Converts inline HTML to a LeafNode
+    '''
     match text_node.text_type:
         case TextType.TEXT:
             return LeafNode(tag=None, value=text_node.text)
@@ -24,6 +89,195 @@ def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
         case _:
             raise Exception("TextNode to HTMLNode Error: invalid TextType given")
 
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    block_list = markdown_to_blocks(markdown)
+
+    children_list = []
+    for block in block_list:
+        block_type = block_to_block_type(block)
+
+        match block_type:
+            case BlockType.PARAGRAPH:
+                node = block_to_paragraph_html_node(block)
+                children_list.append(node)
+            case BlockType.HEADING:
+                node = block_to_heading_html_node(block)
+                children_list.append(node)
+            case BlockType.CODE:
+                node = block_to_code_html_node(block)
+                children_list.append(node)
+            case BlockType.QUOTE:
+                node = block_to_quote_html_node(block)
+                children_list.append(node)
+            case BlockType.ORDERED_LIST:
+                node = block_to_list_html_node(block, "ol")
+                children_list.append(node)
+            case BlockType.UNORDERED_LIST:
+                node = block_to_list_html_node(block, "ul")
+                children_list.append(node)
+
+    root_node = ParentNode("div", children_list)
+    return root_node
+
+def markdown_to_blocks(markdown: str) -> list[str]:
+    blocks = markdown.split("\n\n")
+    
+    # remove surrounding whitespace & filter empty lines
+    blocks = [block_str.strip() for block_str in blocks if block_str.strip() != ""]
+
+    return blocks
+
+#---[ str, TextNode, HTMLNode Conversion ]--------------------------------------
+
+
+#---[ Block str -> HTMLNode Conversion ]----------------------------------------
+def block_to_paragraph_html_node(block: str) -> ParentNode | LeafNode:
+    block = " ".join(block.split("\n"))
+
+    children_list = text_to_children(block)
+    res_node = None
+
+    if not children_list:
+        res_node = LeafNode("p", block)
+    else:
+        res_node = ParentNode("p", children_list)
+
+    return res_node
+
+def block_to_heading_html_node(block: str) -> HTMLNode:
+    pound_count = 0
+
+    for i in range(6):
+        if block[i] != "#":
+            break
+        pound_count += 1
+
+    heading_tag_str = f"h{pound_count}"
+    heading_text = block[pound_count + 1:]
+    # children_list = text_to_text_nodes(heading_text)
+
+    children_list = text_to_children(heading_text)
+
+    heading_node = LeafNode(heading_tag_str, heading_text)
+    if len(children_list) > 1:
+        heading_node = ParentNode(heading_tag_str, children_list)
+
+    return heading_node
+
+def block_to_code_html_node(block: str) -> ParentNode:
+    '''
+    Converts a markdown code block to an HTML <pre><code>...</code></pre> node.
+
+    Preconditions:
+        - `block` starts and ends with exactly three backticks (```). They are two
+          separate groupings.
+
+    Args:
+        - `block` (str): markdown code block string. content is wrapped in triple
+          backticks
+
+    Postconditions:
+        - `ParentNode` returned is always <pre> wrapping `LeafNode` <code>
+
+    Returns:
+        - `pre_node` (`ParentNode`): html node <pre> wrapping `LeafNode` <code>
+    '''
+    res = block[3:-3]
+
+    first_newline = res.find("\n")
+    res = res[first_newline+1:]
+
+    node = TextNode(res, TextType.CODE)
+    node = text_node_to_html_node(node)
+
+    pre_node = ParentNode("pre", [node])
+    return pre_node
+
+# TODO: further parse lines using text -> TextNode -> LeafNode
+def block_to_quote_html_node(block: str) -> LeafNode:
+    lines = block.split('\n')
+    res = [line[2:] for line in lines]
+    quote = "\n".join(res)
+    
+    node = LeafNode("blockquote", quote)
+
+    return node
+
+def block_to_list_html_node(block: str, list_type: str="ul") -> ParentNode | LeafNode:
+    """
+    Converts a markdown unordered list block into an HTML <ul> node 
+    containing <li> elements, with support for bold, italic, and code formatting.
+
+    Preconditions:
+        - Each line in `block` must start with "- " (dash followed by space).
+        - The block represents a valid unordered list in markdown format.
+
+    Args:
+        `block` (str): A markdown string representing an unordered list, where each list item starts on a new line with "- ".
+
+    Postconditions:
+        - Returns a `ParentNode` with tag 'ul' containing one <li> child node 
+          for each item in the markdown list.
+        - Supports nested inline formatting using:
+            - `**bold**` for bold
+            - `_italic_` for italic
+            - `` `code` `` for inline code
+
+    Returns:
+        `ParentNode`: An HTML <ul> element where each child is an <li> element
+        corresponding to a list item in the markdown block.
+
+    Side Effects:
+        None.
+
+    Raises:
+        None.
+    """
+    # removes the "- " at the start of each list element
+    lines = block.split('\n')
+    
+    res = []
+
+    if list_type == "ul":
+        res = [line[2:] for line in lines]
+    elif list_type == "ol":
+        res = [line[3:] for line in lines]
+
+    # collect all of the list element nodes
+    item_node_list = []
+    for line in res:
+        # check for inline elements
+        text_node_list = text_to_text_nodes(line)
+        text_node = TextNode(line)
+
+        # default to child not having nested elements
+        child = LeafNode("li", line)
+    
+        # list has nested elements
+        if text_node_list != [text_node]:
+            leaf_node_list = []
+
+            for node in text_node_list:
+                if node.text:
+                    leaf_node = text_node_to_html_node(node)
+                    leaf_node_list.append(leaf_node)
+ 
+            child = ParentNode("li", leaf_node_list)
+
+        # list element node added
+        item_node_list.append(child)
+ 
+    # create wrapping <ul> element w/ the <li> leaf & parent nodes
+    if list_type in ("ul", "ol"):
+        list_node = ParentNode(list_type, item_node_list)
+        return list_node
+
+    raise ValueError("Error: list_type is invalid type (only ul and ol supported for now)")
+
+#---[ Block str -> HTMLNode Conversion ]----------------------------------------
+
+
+#---[ Split TextNodes & extract link str ]--------------------------------------
 def split_nodes_delimiter(
     node_list: list[TextNode],
     delimiter: str,
@@ -58,6 +312,7 @@ def split_nodes_delimiter(
             num_delimiters += 1
 
         split_res = []
+
         if num_delimiters >= 2:
             split_res = string.split(delimiter, 2)
 
@@ -79,18 +334,6 @@ def split_nodes_delimiter(
             res.append(node)
 
     return res
-
-def extract_markdown_images(text: str) -> list[tuple[str, str]]:
-    pattern = r"!\[(.*?)\]\((.*?)\)"
-    matches = re.findall(pattern, text)
-
-    return matches
-
-def extract_markdown_links(text: str) -> list[tuple[str, str]]:
-    pattern = r"\[(.*?)\]\((.*?)\)"
-    matches = re.findall(pattern, text)
-
-    return matches
 
 def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     new_nodes = []
@@ -182,37 +425,84 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
 
     return new_nodes
 
-def text_to_textnodes(text: str) -> list[TextNode]:
-    type_delimiter_map = {
-        TextType.BOLD: "**",
-        TextType.ITALIC: "_",
-        TextType.CODE: "`"
-    }
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
 
-    node = TextNode(text)
-    node_list = [node]
+    return matches
 
-    for text_type, delimiter in type_delimiter_map.items():
-        node_list = split_nodes_delimiter(node_list, delimiter, text_type)
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
 
-    node_list = split_nodes_image(node_list)
-    node_list = split_nodes_link(node_list)
+    return matches
 
-    return node_list
+#---[ Split TextNodes & extract link str ]--------------------------------------
 
-def test():
-    # test1 = TextNode("image ![middle image](https)middle")
-    # test2 = TextNode("![starting image](https) image start")
-    # test3 = TextNode("image end ![starting image](https)")
-    # test4 = TextNode("here is a kitty: ![kitty placeholder](https://cat-image.com), and another one: ![some cat](https://hotmail.com)b")
-    # test5 = TextNode("![raw image](byitself)")
-    res = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
 
-    for node in res:
-        print(node)
+#---[ Block str Type Checking ]-------------------------------------------------
+def block_to_block_type(markdown_block: str) -> BlockType:
+    '''
+    - precondition: whitespace stripped from markdown_block
+    '''
+    res = BlockType.PARAGRAPH
 
-    return
+    lines = markdown_block.split('\n')
+    if check_block_is_heading(lines):
+        res = BlockType.HEADING
+    elif check_block_is_code(lines):
+        res = BlockType.CODE
+    elif check_block_is_quote(lines):
+        res = BlockType.QUOTE
+    elif check_block_is_unordered_list(lines):
+        res = BlockType.UNORDERED_LIST
+    elif check_block_is_ordered_list(lines):
+        res = BlockType.ORDERED_LIST
 
-if __name__ == "__main__":
-    test()
+    return res
 
+def check_block_is_heading(lines: list[str]) -> bool:
+    import re
+    if len(lines) != 1: return False
+
+    pattern = r"(#{1,6}) ([^\s].*)"
+    return re.match(pattern, lines[0]) is not None
+
+def check_block_is_code(lines: list[str]) -> bool:
+    # check total length is at least 6
+    total_length = 0
+
+    for line in lines:
+        total_length += len(line)
+
+    if total_length < 6:
+        return False
+
+    # check ends are both triple back ticks
+    return lines[0].startswith(r"```") and lines[-1].endswith(r"```") 
+
+def check_block_is_quote(lines: list[str]) -> bool:
+    for line in lines:
+        if not line.startswith(">"):
+            return False
+
+    return True
+
+def check_block_is_unordered_list(lines: list[str]) -> bool:
+    for line in lines:
+        if not line.startswith("- "):
+            return False
+
+    return True
+
+def check_block_is_ordered_list(lines: list[str]) -> bool:
+    count = 1
+
+    for line in lines:
+        if not line.startswith(f"{count}. "):
+            return False
+        count += 1
+
+    return True
+
+#---[ Block str Type Checking ]-------------------------------------------------
